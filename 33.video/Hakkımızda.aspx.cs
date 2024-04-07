d49c976de1d38427319e60f9140dafcf3cc78228
imran4545drn <imran.durna18@hotmail.com>
2024-04-07 14:30:53 +0300

    33.video tamamlandı
    tamamlandı

---
+++ 33.video/Hakkımızda.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+public partial class Hakkımızda : System.Web.UI.Page
+{
+    SqlSinif bgl = new SqlSinif();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlCommand komut = new SqlCommand("SELECT * FROM  Hakkımızda", bgl.baglanti());
+        SqlDataReader oku = komut.ExecuteReader();
+        DataList2.DataSource = oku;
+        DataList2.DataBind();
+        oku.Close();
+    }
+}
---
+++ 33.video/Yemekler.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Yemekler : System.Web.UI.Page
+{
+    SqlSinif bgl = new SqlSinif();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            Panel2.Visible = false;
+            Panel4.Visible = false;
+
+            // Yemekler verisini bağla
+            SqlCommand komut = new SqlCommand("SELECT * FROM Yemekler", bgl.baglanti());
+            SqlDataReader dr = komut.ExecuteReader();
+            DataList1.DataSource = dr;
+            DataList1.DataBind();
+            dr.Close(); // Okuyucuyu kapat
+
+            // Kategoriler verisini bağla
+            SqlCommand komut2 = new SqlCommand("SELECT * FROM Kategoriler", bgl.baglanti());
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            DropDownList1.DataTextField = "KategoriAd";
+            DropDownList1.DataValueField = "Kategoriid";
+            DropDownList1.DataSource = dr2;
+            DropDownList1.DataBind();
+            dr2.Close(); // Okuyucuyu kapat
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Panel2.Visible = true;
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Panel2.Visible = false;
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Panel4.Visible = true;
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        Panel4.Visible = false;
+    }
+}
